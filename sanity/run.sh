baa079f55ea9345eea0d209362e49183389b06ce
Letong Han <hanlt@shanghaitech.edu.cn>
2023-11-16 19:47:43 +0800

    [feat] O3

--- sanity/run.sh
+++ sanity/run.sh
@@ -1,5 +1,5 @@
-/opt/riscv/rv64imf/bin/riscv64-unknown-elf-gcc -I../test $1 ../test/lib.c -o my -ffast-math &&
-/opt/riscv/bin/riscv64-unknown-elf-gcc -I../test $1 ../test/lib_spike.c -o spike -ffast-math &&
+/opt/riscv/rv64imf/bin/riscv64-unknown-elf-gcc -O3 -I../test $1 ../test/lib.c -o my -ffast-math &&
+/opt/riscv/bin/riscv64-unknown-elf-gcc -O3 -I../test $1 ../test/lib_spike.c -o spike -ffast-math &&
 ../build/Simulator my > my.out &&
 spike pk spike -isa=rv64gc > spike_raw.out &&
 tail -n +2 spike_raw.out > spike.tmp && 
